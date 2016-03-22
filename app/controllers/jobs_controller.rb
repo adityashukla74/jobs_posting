40636bd8039ad400fbc5028572b3b50111fbbd95
adityashukla <adityashukla74@gmail.com>
2016-03-22 20:00:05 +0530

    added CRUD abilities

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,13 @@
 class JobsController < ApplicationController
 	before_action :find_job, only: [:show, :edit, :update, :destroy]
+
 	def index
-		@job = Job.all.order("created_at DESC")
+		if params[:category].blank?
+			@jobs = Job.all.order("created_at DESC")
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@jobs = Job.where(category_id: @category_id).order("created_at DESC")
+		end
 	end
 
 	def show
@@ -11,7 +17,6 @@ class JobsController < ApplicationController
 		@job = Job.new
 	end
 
-	
 	def create
 		@job = Job.new(jobs_params)
 
@@ -22,24 +27,29 @@ class JobsController < ApplicationController
 		end
 	end
 
-		def edit
+	def edit
 	end
 
-
 	def update
+		if @job.update(jobs_params)
+			redirect_to @job
+		else
+			render "Edit"
+		end
 	end
 
 	def destroy
+		@job.destroy
+		redirect_to root_path
 	end
 
 	private
 
 	def jobs_params
-		params.require(:job).permit(:title, :description, :company, :url)
+		params.require(:job).permit(:title, :description, :company, :url, :category_id)
 	end
 
 	def find_job
 		@job = Job.find(params[:id])
 	end
-
 end
